ebbf7f6ddc99ad981f93e7b837e85cb7148affac
Harutaka Abe <a5819004@aoyama.jp>
2022-02-04 03:43:16 +0900

    create login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-resources :tasks
+  root   'tasks#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get   '/signup',  to: 'sessions#signup'
+  post   '/signup',  to: 'sessions#signup'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
