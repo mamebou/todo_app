37059d61b01b7b5dc959500a061bf1d7db339eb6
Harutaka Abe <a5819004@aoyama.jp>
2022-02-10 02:44:22 +0900

    create doing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,22 @@ class HomeController < ApplicationController
 
   def state
      @task = Task.find(params[:id])
-     @task.state = "実行中"
-     @task.save
-     redirect_to top_path
+
+
+     if @task.state == "未実行"
+       @task.state = "実行中"
+       @task.save
+       redirect_to top_path
+     elsif @task.state == "実行中"
+       @task.state = "実行済み"
+       @task.save
+       redirect_to doing_path
+     end
+  end
+
+  def doing
+    @tasks = all_tasks
+    @doing_tasks = @tasks.where(user_id:session[:user_id]).where(state:"実行中")
   end
 
 end
