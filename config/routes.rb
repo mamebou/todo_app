8b2c48b7251e77c909afc3ade05fba69d6c8c50c
Harutaka Abe <a5819004@aoyama.jp>
2022-02-10 02:52:32 +0900

    create done page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root  'home#top'
   get   '/top',    to: 'home#top'
   get   '/doing',  to: 'home#doing'
+  get   '/done',   to: 'home#done'
   get   'home/state',   to: 'home#state'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
