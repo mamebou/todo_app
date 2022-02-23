10a885f754641cd6d4c773d034a96135de4e3bc9
Harutaka Abe <a5819004@aoyama.jp>
2022-02-24 01:45:04 +0900

    add culumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_164931) do
+ActiveRecord::Schema.define(version: 2022_02_23_164219) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_164931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "detail"
+    t.integer "board_num"
   end
 
   create_table "users", force: :cascade do |t|
